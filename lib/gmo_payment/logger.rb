965db7eefee866ead42ae4c9863f8c1c354c38a2
Manh Dao Van <manhdaovan@gmail.com>
2017-01-11 14:37:03 +0900

    Separate to module (not test)

--- lib/gmo_payment/logger.rb
+++ lib/gmo_payment/logger.rb
@@ -1,9 +1,11 @@
-class GmoPayment::Logger
-  class << self
-    def write(msg)
-      log_path = GmoPayment::Configurations.all.fetch(:log_path)
-      File.open(log_path, 'a+') do |f|
-        f.write("#{Time.now} :: #{msg} \n")
+module GmoPayment
+  class Logger
+    class << self
+      def write(msg)
+        log_path = GmoPayment::Configurations.all.fetch(:log_path)
+        File.open(log_path, 'a+') do |f|
+          f.write("#{Time.now} :: #{msg} \n")
+        end
       end
     end
   end
